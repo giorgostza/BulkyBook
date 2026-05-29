e4ee40413d7341f6068302cb90f4f230505ee461
ΓΙΩΡΓΟΣ ΤΖΑΒΕΛΛΑΣ <giorgostza91@gmail.com>
2026-05-29 15:59:26 +0300

    Create Category Model Connection String Nugget Packages ApllicationDbContext Seeding Category Create Category Controller Create Category Index Modyfying CSS File and Images File

--- BulkyBookWeb/Controllers/HomeController.cs
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using BulkyBookWeb.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -16,10 +16,6 @@ namespace BulkyBookWeb.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
